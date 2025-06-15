4005fbd36371df7a4b3f78b3942049fd17650479
Cutie Deng <CutieDeng@iCloud.com>
2025-06-15 22:40:38 +0800

    (FEAT "ral: safe guard")

--- ral.rkt
+++ ral.rkt
@@ -6,28 +6,30 @@
 (define-match-expander ral (#%plain-lambda (stx)
   (syntax-case stx (length unlength atom)
     [(_ (hd length sz))
-      (syntax (? (lambda (s) (= (ral-length s) sz)) hd))
+      (syntax (? ral? (? (lambda (s) (= (ral-length s) sz)) hd)))
     ]
     [(_ (hd unlength))
       (syntax (? ral? hd))
     ]
     [(_ (hd atom))
-      (syntax (? (lambda (s) (= (ral-length s) 1)) (app ral-viewl hd)))]
+      (syntax (? ral? (? (lambda (s) (= (ral-length s) 1)) (app ral-viewl hd))))
+    ]
     [(self (hd length sz) rest ...)
-      (syntax (? (lambda (s) (>= (ral-length s) sz)) (app (lambda (s) (let-values ([(lhs rhs) (ral-split-at s sz)]) (cons lhs rhs))) (cons hd (self rest ...)))))
+      (syntax (? ral? (? (lambda (s) (>= (ral-length s) sz)) (app (lambda (s) (let-values ([(lhs rhs) (ral-split-at s sz)]) (cons lhs rhs))) (cons hd (self rest ...))))))
     ]
     [(self (hd unlength) rest ... (rhd length sz)) 
-      (syntax (? (lambda (s) (>= (ral-length s) sz)) (app (lambda (s) (let-values ([(rhs lhs) (ral-split-at-right s sz)]) (cons lhs rhs))) (cons (self (hd unlength) rest ...) rhd))))
+      (syntax (? ral? (? (lambda (s) (>= (ral-length s) sz)) (app (lambda (s) (let-values ([(rhs lhs) (ral-split-at-right s sz)]) (cons lhs rhs))) (cons (self (hd unlength) rest ...) rhd)))))
     ]
     [(self (hd atom) rest ...)
-      (syntax (app (lambda (s) (let-values ([(v ft) (ral-dropl s)]) (cons v ft))) (cons (? (lambda (x) x) hd) (self rest ...))))
+      (syntax (? ral? (app (lambda (s) (let-values ([(v ft) (ral-dropl s)]) (cons v ft))) (cons (? (lambda (x) x) hd) (self rest ...)))))
     ]
     [(self (hd unlength) rest ... (rhd atom))
-      (syntax (app (lambda (s) (let-values ([(v ft) (ral-dropr s)]) (cons v ft))) (cons (? (lambda (x) x) rhd) (self (hd unlength) rest ...))))
+      (syntax (? ral? (app (lambda (s) (let-values ([(v ft) (ral-dropr s)]) (cons v ft))) (cons (? (lambda (x) x) rhd) (self (hd unlength) rest ...)))))
     ]
-    [(_ (_ unlength) rest ... (_ unlength))
+    [(_ (_ unlength) _rest ... (_ unlength))
       (error 'unlength "unsupport complex unlength matching")
     ]
+    [(_) (syntax (? ral? (? ral-empty?)))]
   )))
 
 (provide ral)
